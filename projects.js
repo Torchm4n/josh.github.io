039b062f7ee92935249a92a96149aa8496d70918
zc015821 <joshedporch@gmail.com>
2026-01-08 23:25:14 +0000

    More projects page stuff

--- projects.js
+++ projects.js
@@ -1,26 +1,27 @@
-var index = 0;
+document.addEventListener("DOMContentLoaded", async () => {
+    var index = 0;
 
-const data_promise = fetch("./data.json").then(r => r.json()).then(obj => Object.values(obj));
-const length_promise = data_promise.then(d => d.length);
+    const data_promise = fetch("./data.json").then(r => r.json()).then(obj => Object.values(obj));
+    const length_promise = data_promise.then(d => d.length);
 
-const month = document.querySelector("#projects h1");
-const title = document.querySelector("#projects #title")
-const description = document.querySelector("#projects #description");
-const link = document.querySelector("#projects #link #alink");
+    const month = document.getElementById("month");
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    const link = document.getElementById("alink");
 
-const data = await data_promise;
-const length = await length_promise;
+    const data = await data_promise;
+    const length = await length_promise;
 
-month.textContent = data[index]["month"];
-title.textContent = data[index]["title"];
-description.textContent = data[index]["description"];
-link.textContent = data[index]["link"];
+    title.textContent = data[index].title;
+    description.textContent = data[index].description;
+    link.textContent = data[index].link;
 
-document.addEventListener("pointerdown", async () => {
-    month.textContent = data[index]["month"];
-    title.textContent = data[index]["title"];
-    description.textContent = data[index]["description"];
-    link.textContent = data[index]["link"];
+    document.addEventListener("click", async () => {
+        index = (index + 1) % length;
 
-    index = (index + 1) % length;
+        month.textContent = data[index].month;
+        title.textContent = data[index].title;
+        description.textContent = data[index].description;
+        link.textContent = data[index].link;
+    });
 });
