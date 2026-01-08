85ff219b52330253590761b6aec1aa5025ab2dd6
zc015821 <joshedporch@gmail.com>
2026-01-08 22:55:50 +0000

    Added to the projects page

---
+++ projects.js
@@ -0,0 +1,26 @@
+var index = 0;
+
+const data_promise = fetch("./data.json").then(r => r.json()).then(obj => Object.values(obj));
+const length_promise = data_promise.then(d => d.length);
+
+const month = document.querySelector("#projects h1");
+const title = document.querySelector("#projects #title")
+const description = document.querySelector("#projects #description");
+const link = document.querySelector("#projects #link #alink");
+
+const data = await data_promise;
+const length = await length_promise;
+
+month.textContent = data[index]["month"];
+title.textContent = `Title: ${data[index]["title"]}`;
+description.textContent = `Description: ${data[index]["description"]}`;
+link.textContent = data[index]["link"];
+
+document.addEventListener("pointerdown", async () => {
+    month.textContent = data[index]["month"];
+    title.textContent = `Title: ${data[index]["title"]}`;
+    description.textContent = `Description: ${data[index]["description"]}`;
+    link.textContent = data[index]["link"];
+
+    index = (index + 1) % length;
+});
